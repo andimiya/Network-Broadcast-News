92adb527a341a16393153188624169c0ad036ca3
Andrea <takamiya.andrea@gmail.com>
2016-12-26 14:31:06 -1000

    Message counts are displaying on the server when clients send messages

--- client.js
+++ client.js
@@ -5,13 +5,13 @@ const net = require('net');
 var server = net.createConnection(6969, 'localhost');
 server.on('connect', () => {
 
-  console.log('connected');
-
   process.stdin.pipe(server);
-
   process.stdin.on('data',(clientMessage) => {
+
     console.log(clientMessage);
     server.write(clientMessage);
+
+
   });
 
 });
--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const net = require('net');
 
+var clientsCount = 0;
+var messageCount = 0;
 
-var server = net.createServer((socket) => {
+var server = net.createServer((socket) => {  //THIS SOCKET is different for every new client connection
 
   socket.on('data', (clientMessage) => {
-    console.log(clientMessage);
+    messageCount++;
+    socket.write(`the current message count is: ${clientsCount}`);
+
+    console.log(`chunk from client: ${clientMessage}`);
+    console.log(`the current message count is: ${messageCount}`);
   });
 
   process.stdin.on('data', (cmd) => {
     socket.write(cmd);
   });
-
 });
 
 server.listen(6969, '0.0.0.0', () => {
