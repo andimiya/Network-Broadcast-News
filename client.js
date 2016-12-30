889aa8a83c6456b1beccabc13b2b34ecb538b88c
Andrea <takamiya.andrea@gmail.com>
2016-12-29 15:41:37 -1000

    Removed all the code that didn't work when I was trying to add an admin and usernames, removed console logs, cleaned up and extra comments. MVP.

--- client.js
+++ client.js
@@ -3,19 +3,12 @@ const net = require('net');
 // v--------socket connection to server
 
 var server = net.createConnection(6969, 'localhost',() => {
-  console.log('Connected to:');
-  // process.stdout.write('Enter your username:');
+  process.stdout.write(`Connected to: ${server.remoteAddress}\n`);
 });
 
 server.on('connect', () => {
   process.stdin.pipe(server);
 
   server.on('data', (serverMessage) => {
-    process.stdout.write(serverMessage);
-
-  });
-    server.on('data', (adminMessage) => {
-    process.stdout.write(adminMessage);
-
   });
 });
--- server.js
+++ server.js
@@ -12,7 +12,6 @@ clientsCount++;
 
   //Identify the client that just joined
   var socketAddress = socket.remoteAddress;
-  socket.id = Math.floor(Math.random() * 1000);
 
   //Add the socket to the sockets Array
   sockets.push(socket);
@@ -22,51 +21,26 @@ clientsCount++;
 
   socket.on('data', (clientMessage) => {
     messageCount++;  //Running count of all messages
-    socket.username = clientMessage;
-    console.log(socketAddress, 'socket username');
-    if (socket.username === 'andrea') {
-      process.stdout.write('authenticated');
-    }
-    else {
-      console.log('not authenticated');
-    }
 
     broadcast(clientMessage, socket);  //Broadcast this to all sockets
 
-    // console.log(`the current message count is: ${messageCount}`);
-    // console.log(`number of clients connected: ${clientsCount}`);
-    // console.log(`User ${socket.id} said: ${clientMessage}`);
+    process.stdout.write(`the current message count is: ${messageCount}\n`);
+    process.stdout.write(`number of clients connected: ${clientsCount}\n`);
+    process.stdout.write(`User ${socketAddress} said: ${clientMessage}\n`);
   });
 
   //If there are 3 sockets open, take the socketID from the client sending the message, and pass the message to the other 2 sockets
   //Loop through all of the sockets and send the message to all of the sockets
   function broadcast(message, sender){
     for (var i = 0; i < sockets.length; i++) {
-      //Try this out:
       if(sockets[i] !== socket) {
-        sockets[i].write(`${socketAddress}: ${message}`);
+        sockets[i].write(`User ${socketAddress} said: ${message}`);
+        console.log(socket);
       }
     }
   }
 });
 
-    // sockets.forEach(function (client) {
-    //   // Don't send it to sender
-    //   if (client === sender) return;
-    //   client.write(`message from ${socket.id}: ${message}`);
-    // });
-    // process.stdout.write(`message from ${socket.id}: ${message}`);
-  // }
-
 server.listen(6969, '0.0.0.0', () => {
-  console.log('opened server on', server.address());
+  process.stdout.write('opened server on', server.address());
 });
-
-
-
-
-//<----NOTE----->
-//When someone connects, add an event listener named data and send the message from the server to this 1 client
-  // process.stdin.on('data', (cmd) => {
-  //   socket.write(cmd);
-  // });
